7c41577653da0ee7bfdc981ab5f9adad1bef833a
el_codee <elfrand3@gmail.com>
2022-11-09 23:16:36 +0700

    belajar kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    val nama : String = "Genji"
+    val umur : Int = 20
+    var sekolah = "UNUJA"
+    var alamat : String = "paiton"
+    alamat = "tanjung"
+    var golD = '0'
+
+    print("nama saya $nama, umur saya $umur, \nsekolah saya di $sekolah, alamat saya di $alamat, golongan darah saya $golD, \n");
+
+    print(nama );
+
+    println(umur);
+}
---
+++ src/main/kotlin/percabangan.kt
@@ -0,0 +1,39 @@
+fun main(){
+
+//    case 1
+    var nilaiA = 30
+    var nilaiB = 15
+
+    if(nilaiA < nilaiB){
+        println("salah")
+    }else{
+        println("benar")
+    }
+
+//    case 2
+    val number = -10
+    if(number > 0){
+        println("nilai positif")
+    }else{
+        println("nilai negatif")
+    }
+
+//    case 3
+//    val nilai = 8
+//
+//    if(nilai % 2 == 0){
+//        println("genap")
+//    }else{
+//        println("ganjil")
+//    }
+
+    val nilai = 7
+
+    if(nilai % 2 == 1){
+        println("ganjil")
+    }else{
+        println("genap")
+    }
+
+
+}
